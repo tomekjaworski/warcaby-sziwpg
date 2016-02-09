430a6e80c4c82dbfd66ed86a4bfdc61182069270
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2016-02-09 14:06:50 +0100

    Dodanie klasy-magazynu VirtualBoard

--- Checkers/MainWindow.cs
+++ Checkers/MainWindow.cs
@@ -54,7 +54,7 @@ namespace Checkers
                     this.field_matrix[r, c] = this.panel1.Controls[cname] as Label;
 
                     this.normal_color_matrix[r, c] = (r + c) % 2 == 0 ? Color.White : Color.Silver;
-                    this.selected_color_matrix[r, c] = (r + c) % 2 == 0 ? Color.FromArgb(255-50, 255, 255-50) : Color.FromArgb(192-50, 192, 192-50);
+                    this.selected_color_matrix[r, c] = (r + c) % 2 == 0 ? Color.FromArgb(255 - 50, 255, 255 - 50) : Color.FromArgb(192 - 50, 192, 192 - 50);
 
                     this.field_matrix[r, c].BackColor = this.normal_color_matrix[r, c];
                 }
@@ -72,14 +72,15 @@ namespace Checkers
             try
             {
                 this.bot.Initialize();
-            } catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 MessageBox.Show(string.Format("Wyjątek podczas uruchamiania metody Bot.Initialize():\n{0}", ex.Message),
                     Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-      
+
         private bool CheckStopConditions()
         {
             if (this.dbg.chkDontCheckStopConditions.Checked)
@@ -89,7 +90,7 @@ namespace Checkers
             // policz mozliwe ruchy
             int cwhite = 0, cblack = 0;
             int move_counter = 0;
-            for(int r = 0; r < 8;r++)
+            for (int r = 0; r < 8; r++)
                 for (int c = 0; c < 8; c++)
                 {
                     if (Pawn.GetColor(this.pawn_matrix[r, c]) == PawnColor.Black) cblack++;
@@ -437,7 +438,7 @@ namespace Checkers
             if (Pawn.IsQueenPawn(this.GetSelectedPawn()))
             {
                 AddPlayerLog(string.Format("Ruch z {0} na {1} (dama)", Pawn.PointToFieldAddress(pselected), Pawn.PointToFieldAddress(pdest)));
-                
+
                 Point p = pselected;
                 int cap_counter = 0;
                 for (int i = 0; i < Math.Abs(pselected.X - pdest.X); i++)
@@ -505,7 +506,7 @@ namespace Checkers
             }
 
             return avail_moves.Contains(pdest);
-          
+
         }
 
         public void DeselectPawn()
@@ -717,8 +718,8 @@ namespace Checkers
             try
             {
 #endif
-                this.bot.MakeMove();
-                this.EndTurn(false);
+            this.bot.MakeMove();
+            this.EndTurn(false);
 #if DEBUG && false
             }
             catch (Exception ex)
@@ -895,6 +896,410 @@ namespace Checkers
 
 
 
+
+    public class VirtualBoard : IBoard
+    {
+        MainWindow base_form;
+        DebugWindow dbg;
+
+
+        PawnType[,] pawn_matrix;
+        private Point selected_field;
+        private int move_count, capture_count; // TODO: nie wiem, gdzie te pola są zerowane!!!!
+
+        public VirtualBoard(MainWindow base_form, DebugWindow dbg, PawnType[,] pawn_matrix)
+        {
+            this.base_form = base_form;
+            this.dbg = dbg;
+            this.pawn_matrix = pawn_matrix.Clone() as PawnType[,];
+        }
+
+        #region OK
+
+        public void AddPlayerLog(string msg)
+        {
+            this.base_form.AddPlayerLog(msg);
+        }
+
+        public void AddSystemLog(string msg)
+        {
+            this.base_form.AddSystemLog(msg);
+        }
+
+        public bool CanMoveSelectedPawnTo(string field_address)
+        {
+
+            Point pselected = this.selected_field;
+            Point pdest = Pawn.FieldAddressToPoint(field_address);
+
+            if (pselected.IsInvalid())
+                throw new GameException("Nie wybrano pionu metodą SelectPawn(). Nie możesz użyć metody CanMoveSelectedPawnTo()");
+
+            // cel musi być pustym polem
+            if (!Pawn.IsNone(this.GetPawn(field_address)))
+                return false;
+
+            // pobierz wszystkie mozliwe ruchy dla piona
+            Point[] simple_moves = this.GetMovementCoordinates(Pawn.PointToFieldAddress(pselected), PawnType.None); // ruchy bez bicia
+            Point[] capture_moves = this.GetCaptureCoordinates(Pawn.PointToFieldAddress(pselected), PawnType.None); // ruchy z biciem
+            Point[] avail_moves = simple_moves.Union(capture_moves).Distinct().ToArray(); //  C# fajny jest :)
+
+            if (true)
+            {
+                Console.WriteLine("CanMoveSelectedPawnTo(): Pion {0} z {1} na {2}", this.GetPawn(pselected), Pawn.PointToFieldAddress(pselected), field_address);
+                Console.WriteLine("  GetMovementCoordinates() = [{0}]", string.Join(", ", Pawn.PointToFieldAddress(simple_moves)));
+                Console.WriteLine("  GetCaptureCoordinates() = [{0}]", string.Join(", ", Pawn.PointToFieldAddress(capture_moves)));
+            }
+
+            return avail_moves.Contains(pdest);
+
+
+        }
+
+        public void DeselectPawn()
+        {
+            if (this.selected_field.IsInvalid())
+                return;
+
+            this.selected_field = PointExt.Invalid;
+        }
+
+        public string[] GetCaptureFields(string field_address)
+        {
+            return Pawn.PointToFieldAddress(this.GetCaptureCoordinates(field_address, PawnType.None));
+        }
+
+        public string[] GetCaptureFields(string field_address, PawnType hypotetical_pawn_type)
+        {
+            return Pawn.PointToFieldAddress(this.GetCaptureCoordinates(field_address, hypotetical_pawn_type));
+        }
+
+        public PawnType[,] GetCheckboard()
+        {
+            PawnType[,] copy = this.pawn_matrix.Clone() as PawnType[,];
+            return copy;
+        }
+
+
+        public string[] GetMovementFields(string field_address)
+        {
+            return Pawn.PointToFieldAddress(this.GetMovementCoordinates(field_address, PawnType.None));
+        }
+
+        public string[] GetMovementFields(string field_address, PawnType hypothetical_pawn_type)
+        {
+            return Pawn.PointToFieldAddress(this.GetMovementCoordinates(field_address, hypothetical_pawn_type));
+        }
+
+        public PawnType GetPawn(string field_address)
+        {
+            Point p = Pawn.FieldAddressToPoint(field_address);
+            return this.GetPawn(p);
+        }
+
+        private PawnType GetPawn(Point p, PawnType default_type)
+        {
+            if (!Pawn.InBound(p))
+                return default_type;
+
+            return GetPawn(p);
+        }
+
+        private PawnType GetPawn(Point p)
+        {
+            if (!Pawn.InBound(p))
+                throw new GameException("Niepoprawne współrzędne piona");
+            return pawn_matrix[p.Y, p.X];
+        }
+
+        #endregion
+
+
+
+
+        public PawnType GetSelectedPawn()
+        {
+            if (this.selected_field.IsInvalid())
+                return PawnType.None;
+
+            return pawn_matrix[this.selected_field.Y, this.selected_field.X];
+        }
+
+        public bool IsCaptureAvailable(string field_address)
+        {
+            return GetCaptureCoordinates(field_address, PawnType.None).Length > 0;
+        }
+
+        public PawnMoveResult MoveSelectedPawnTo(string field_addr)
+        {
+            field_addr = Pawn.NormalizeFieldAddress(field_addr);
+            // czy gracz próbuje wykonać drugi ruch?
+            if (this.move_count > 0)
+                throw new GameException("XXXXXXXXXXNie można wykonać dwóch ruchów!");
+
+            // można wykonać wiele bić - po biciu nie zmienia się runda (gracz)
+            // ale po biciu nie można wykonać ruchu
+            if (this.capture_count > 0)
+            {
+                string[] arr = this.GetCaptureFields(Pawn.PointToFieldAddress(this.selected_field));
+                if (!arr.Contains(field_addr))
+                    throw new GameException("XXXXXXXXXXXXXXPo udanym biciu można wykonać tylko kolejne; nie można wykonać zwykłego ruchu!");
+            }
+
+
+            if (!CanMoveSelectedPawnTo(field_addr))
+                return PawnMoveResult.Nothing;
+
+            Point pselected = this.selected_field;
+            Point pdest = Pawn.FieldAddressToPoint(field_addr);
+
+            Point delta = pdest.Subtract(pselected);
+
+            if (Pawn.IsNormalPawn(this.GetSelectedPawn())) // czy na wybranym polu stoi zwykły pion (nie dama)
+            {
+                if ((Math.Abs(delta.X) == 1 && Math.Abs(delta.Y) == 1)) // przesun pion
+                {
+                    this.move_count++;
+                    this.InternalMovePawn(pdest, pselected);
+                    AddPlayerLog(string.Format("XXXXXXXXXXXXXXXRuch z {0} na {1} (pion)", Pawn.PointToFieldAddress(pselected), Pawn.PointToFieldAddress(pdest)));
+
+                    /////////////// this.ShowGameState();
+                    return PawnMoveResult.NormalMove;
+                }
+
+                if ((Math.Abs(delta.X) == 2 && Math.Abs(delta.Y) == 2)) // bicie
+                {
+                    this.capture_count++;
+                    this.InternalMovePawn(pdest, pselected);
+
+                    // usun piona przeciwnika i dodaj punkt graczowi
+                    Point mid_point = pdest.Midpoint(pselected);
+                    this.pawn_matrix[mid_point.Y, mid_point.X] = PawnType.None;
+
+                    AddPlayerLog(string.Format("XXXXXXXXXXXBicie z {0} na {1} (pion)", Pawn.PointToFieldAddress(pselected), Pawn.PointToFieldAddress(pdest)));
+                    ///////////////this.ShowGameState();
+                    return PawnMoveResult.Capture;
+                }
+
+                throw new Exception("XXXXXXXXXXXXBłąd współrzędnych ruchu; to się nie powinno zdarzyć");
+            }
+
+            if (Pawn.IsQueenPawn(this.GetSelectedPawn()))
+            {
+                AddPlayerLog(string.Format("XXXXXXXXXXXXXXXRuch z {0} na {1} (dama)", Pawn.PointToFieldAddress(pselected), Pawn.PointToFieldAddress(pdest)));
+
+                Point p = pselected;
+                int cap_counter = 0;
+                for (int i = 0; i < Math.Abs(pselected.X - pdest.X); i++)
+                {
+                    // przejdź o jedno pole w kierunku pdest
+                    p = new Point(p.X + Math.Sign(delta.X), p.Y + Math.Sign(delta.Y));
+
+                    if (!Pawn.IsNone(this.GetPawn(p))) // Zdejmuj pion przeciwnika!
+                    {
+                        this.pawn_matrix[p.Y, p.X] = PawnType.None;
+                        cap_counter++;
+                        AddPlayerLog(string.Format("XXXXXXXXXXXXXXXXBicie na {0} (dama)", Pawn.PointToFieldAddress(p)));
+                    }
+                }
+
+                if (cap_counter > 0)
+                    this.capture_count += capture_count;
+                else
+                    this.move_count++;
+
+                this.InternalMovePawn(pdest, pselected);
+                ///////////////////////////////this.ShowGameState();
+
+                // punkty
+                //if (cap_counter > 0)
+                //{
+                //    if (this.current_turn == PawnColor.Black)
+                //        this.black_score += cap_counter * 2;
+                //    else
+                //        this.white_score += cap_counter * 2;
+
+                //    return PawnMoveResult.Capture;
+                //}
+
+                return PawnMoveResult.NormalMove;
+            }
+
+            // błąd
+            Debug.Assert(false);
+            return PawnMoveResult.Nothing;
+
+        }
+
+        public PawnType SelectPawn(string field_address)
+        {
+            PawnType pc = this.GetPawn(field_address);
+
+            //if (!Pawn.EqualColor(this.current_turn, pc)) // czy próba wybrania nie swojego piona?
+            //{
+            //    System.Media.SystemSounds.Hand.Play();
+            //    return PawnType.None;
+            //}
+
+            //
+
+            Point p = Pawn.FieldAddressToPoint(field_address);
+
+            if (p == this.selected_field)
+                return pawn_matrix[p.Y, p.X];
+
+            DeselectPawn();
+
+            PawnType cl = pawn_matrix[p.Y, p.X];
+            if (cl != PawnType.None)
+            {
+                this.selected_field = p;
+                ////this.field_matrix[p.Y, p.X].BorderStyle = BorderStyle.FixedSingle;
+                ////this.field_matrix[p.Y, p.X].BackColor = this.selected_color_matrix[p.Y, p.X];
+            }
+
+            return cl;
+        }
+
+
+        //////////////////////
+
+        public Point[] GetMovementCoordinates(string field_address, PawnType hypotetical_pawn_type)
+        {
+            Point pselected = Pawn.FieldAddressToPoint(field_address);
+            List<Point> directions = new List<Point>();
+
+            // jeżeli typ hipotetycznego piona nie jest pusty (none) to udaj, że PT(pselected) == hipotetyczny
+            PawnType pt_selected = this.GetPawn(pselected);
+            if (hypotetical_pawn_type != PawnType.None)
+                pt_selected = hypotetical_pawn_type;
+
+            if (Pawn.IsNormalPawn(pt_selected))
+            {
+                // sprawdź wszystkie kierunki dla piona
+                Point[] deltas;
+                if (Pawn.GetColor(pt_selected) == PawnColor.White)
+                {
+                    // białe - tylko w dół
+                    deltas = new Point[] { new Point(1, 1), new Point(-1, 1) };
+                }
+                else
+                {
+                    // czarne - tylko w górę
+                    deltas = new Point[] { new Point(1, -1), new Point(-1, -1) };
+                }
+
+                if (this.dbg.chkAllowNormalPawnMoveBack.Checked) // hack :)
+                    deltas = new Point[] { new Point(1, 1), new Point(1, -1), new Point(-1, -1), new Point(-1, 1) }; // wszystkie cztery przekątne
+
+                // w najbliższym sąsiedztwie musi być wolne pole
+                foreach (Point delta in deltas)
+                {
+                    Point pn = pselected.Add(delta);
+                    if (Pawn.IsNone(this.GetPawn(pn, PawnType.None)) && Pawn.InBound(pn))
+                        directions.Add(pn);
+                }
+            }
+
+            if (Pawn.IsQueenPawn(pt_selected))
+            {
+                // sprawdź wszystkie kierunki dla damy
+                foreach (Point delta in new Point[] { new Point(1, 1), new Point(1, -1), new Point(-1, -1), new Point(-1, 1) })
+                {
+                    // mozna bić, jeśli pion przeciwnika ma wolne pole po nim (przed może stać dama)
+                    Point p = pselected;
+                    p = p.Add(delta);
+                    while (Pawn.InBound(p))
+                    {
+                        PawnType pt = this.GetPawn(p);
+                        if (!Pawn.IsNone(pt))
+                            break; // już tutaj i dalej dama nie może się poruszać
+
+                        directions.Add(p);
+                        p = p.Add(delta); // przejdź o jedno pole w kierunku pdest
+                    }
+                }
+            }
+            return directions.ToArray();
+        }
+
+
+        public Point[] GetCaptureCoordinates(string field_address, PawnType hypotetical_pawn_type)
+        {
+            Point pselected = Pawn.FieldAddressToPoint(field_address);
+            List<Point> directions = new List<Point>();
+
+            // jeżeli typ hipotetycznego piona nie jest pusty (none) to udaj, że PT(pselected) == hipotetyczny
+            PawnType pt_selected = this.GetPawn(pselected);
+            if (hypotetical_pawn_type != PawnType.None)
+                pt_selected = hypotetical_pawn_type;
+
+            if (Pawn.IsNormalPawn(pt_selected))
+            {
+                // w najbliższym sąsiedztwie musi być pion/dama o przeciwnym kolorze oraz następne pola muszą być puste
+                foreach (PawnColor pc in new PawnColor[] { PawnColor.White, PawnColor.Black })
+                    foreach (Point delta in new Point[] { new Point(1, 1), new Point(1, -1), new Point(-1, -1), new Point(-1, 1) })
+                    {
+                        Point pn = pselected.Add(delta);
+                        Point pnn = pn.Add(delta);
+                        if (!Pawn.InBound(pn) || !Pawn.InBound(pnn))
+                            continue;
+                        if ((Pawn.GetColor(pt_selected) == pc) &&
+                            (Pawn.GetColor(this.GetPawn(pn, PawnType.None)) == Pawn.GetOpponentColor(pc)) &&
+                            (Pawn.IsNone(this.GetPawn(pnn, PawnType.None))))
+                            directions.Add(pnn);
+                    }
+            }
+
+            if (Pawn.IsQueenPawn(pt_selected))
+            {
+                PawnColor my_color = Pawn.GetColor(pt_selected);
+                PawnColor opponent_color = Pawn.GetOpponentColor(my_color);
+
+                // dama może bić w każdym kierunku
+                foreach (Point delta in new Point[] { new Point(1, 1), new Point(1, -1), new Point(-1, -1), new Point(-1, 1) })
+                {
+                    Point p = pselected;
+                    bool opponent_pawn_found = false;
+                    int capture_count = 0; // licznik zbić dam/pionów przeciwnika
+                    p = p.Add(delta);
+                    while (Pawn.InBound(p))
+                    {
+                        PawnType pt = this.GetPawn(p);
+
+                        // jeśli miejsce jest puste a coś udało mi się zbić, to mogę tutaj postawić damę
+                        if (Pawn.IsNone(pt) && (capture_count > 0))
+                            directions.Add(p);
+
+                        // dama nie może przeskoczyć nad własnym pionem (tego samego koloru)
+                        if (Pawn.GetColor(pt) == my_color)
+                            break;
+
+                        if (!Pawn.IsNone(pt) && opponent_pawn_found) // dwa piony przciwnika pod rząd - nie można bić
+                            break;
+                        opponent_pawn_found = !Pawn.IsNone(pt);
+
+                        if (opponent_pawn_found)
+                            capture_count++;
+
+                        p = p.Add(delta); // przejdź o jedno pole w kierunku pdest
+                    }
+                }
+            }
+
+            return directions.ToArray();
+        }
+
+
+        private void InternalMovePawn(Point dest, Point source)
+        {
+            Debug.Assert(Pawn.InBound(dest) && Pawn.InBound(source));
+            PawnType pc = this.pawn_matrix[source.Y, source.X];
+            this.pawn_matrix[source.Y, source.X] = PawnType.None;
+            this.pawn_matrix[dest.Y, dest.X] = pc;
+        }
+
+    }
 
 
 }
